829f9d5d77ce402f7fa012e8e830039b58086196
mmn42643-jpg <mmn42643@gmail.com>
2025-11-19 08:40:44 +0300

    Import dotenv/config for environment variable loading

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import fetch from 'node-fetch';
+import 'dotenv/config';
 
 dotenv.config();
 const app = express();
